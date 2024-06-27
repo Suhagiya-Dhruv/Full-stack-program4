df35881a4e60b4eadb439f28b4623a4f317f5f90
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-06-27 17:11:40 +0530

    JS Function

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-## Task
+Function Declaration 
 
-1. user give a array of [1,5,7,8,9,10,15,748,12,54,840,21,8,84,058,54,87] and you need to calculate odd numbers sum and even numbers sum and print them out to the screen.
+Named function
 
-2. user give a list of [1,-9,87,-48,-93,-817,-8148,848,287,287,-84,848,-12,48,36] and you need to calculate positive numbers sum and negative numbers sum and print them
+Anonymous Functions / Expression Functions
+
+IIFI Function (immediately invoked function expression)
+
+Method
+
+this keyword
+
+Parameters and agrument
+
--- script.js
+++ script.js
@@ -1,25 +1,147 @@
+// Named Function
+
+// function sum(){ // Function Declaration
+//   console.log("Sum function");
+// }
+
+// sum() // Call
+
+// function sum(a){ // Argument
+//   console.log(a);
+// }
 
 
-var a = [1, 2, 15, 1, 90, 15, 20, 74, 17, 20, 15, 20, 75];
-var b = 1;
-var c = 0;
+// sum(10) // Parameter
 
-for(var i=0;i<a.length;i++){
-  if(a[i] == b){
-    c = c+1;
-  }
-}
+// function sum(a = 1) { // Default value set 
+//   console.log(a);
+// }
 
-console.log(c);
-
+// sum(10) // Parameter
+// sum(15)
+// sum()
+// sum("hello")
 
 
-var a = [1, 2, 15, 1, -90, 15, -20, 74, 17, -20, 15, 20, 75];
-var c = 0;
+// function sum(sum1, sum2) { // Default value set 
+//   console.log(sum1, sum2);
+// }
 
-for(var i=0;i<a.length;i++){
-  console.log(c);
-    c = c +a[i]
-}
+// sum(10, 15)
 
-console.log(c);
+
+// Anonymous Functions / Expression Functions
+
+// var minus = function (a) {
+//   console.log(a)
+// }
+
+// minus(10)
+
+// IIFI Function (immediately invoked function expression)
+
+
+// (function (a) {
+//   console.log("IIFI Function",a)
+// })(10)
+
+
+// function sum() {
+//   console.log("Function Called")
+//   return 15;
+// }
+
+// console.log(sum())
+
+// user give me two value 10 15 and we need to return sum of the number
+
+
+// var a = 20;
+// var b = 20;
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// var ans = sum(a, b)
+// console.log(ans);
+
+// function sum() {
+
+// }
+
+// sum() // Function Call
+
+// Method Call
+
+// var a = {
+//   name: "john",
+//   minus: function () {
+//     console.log("Method Called", this,a)
+//   }
+// }
+
+// a.minus() // Method Call
+
+// var b = a.minus; // 
+
+// b() // Function   
+
+
+// console.log(window.a)
+// console.log(this.a)
+
+
+// var lname = "Sharma"
+
+// var obj = {
+//   fname: "John",
+//   // lname: "Rai",
+//   fn: function () {
+//     console.log(this.fname + " " +this.lname);
+//   }
+// }
+
+// obj.fn() // John Rai
+// var newFn = obj.fn;
+
+// newFn() // Undefined Sharma
+
+
+// ES6 / 2015
+
+var a = 15;
+
+a = 20;
+
+var a = 35;
+
+// console.log(a);
+
+let b = 15;
+
+b = 20;
+
+// let b = 35; (X)
+
+// console.log(b)
+
+const c = 26;
+
+// c = 96; (X)
+
+// const c = 35; (X)
+
+// console.log(c);
+
+// == vs ===
+
+
+// const cba = "5"
+// const abc = 5;
+
+
+// console.log(cba == abc) // value
+// console.log(cba === abc) // value and type
+
+// Block scope
