d417d5af4ce678448574e0b29fe78edf65dd228e
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-06-29 18:19:13 +0530

    DSA

--- script.js
+++ script.js
@@ -1,147 +1,60 @@
-// Named Function
+var a = 10;
+var b = 20;
 
-// function sum(){ // Function Declaration
-//   console.log("Sum function");
-// }
+a = a*b; // 200
 
-// sum() // Call
+b = a/b; // 200/20 -> 10
+a = a/b; // 200/10 -> 20
 
-// function sum(a){ // Argument
-//   console.log(a);
-// }
+console.log("A", a);
+console.log("B", b);
 
-
-// sum(10) // Parameter
-
-// function sum(a = 1) { // Default value set 
-//   console.log(a);
-// }
-
-// sum(10) // Parameter
-// sum(15)
-// sum()
-// sum("hello")
+// =====================================================
 
 
-// function sum(sum1, sum2) { // Default value set 
-//   console.log(sum1, sum2);
-// }
+var a = "ihelilo this is js lacture";
+var b = "i"
 
-// sum(10, 15)
+var c = a.split(b)
 
+console.log(c.length-1)
 
-// Anonymous Functions / Expression Functions
+// console.log(a.split(""))
 
-// var minus = function (a) {
-//   console.log(a)
-// }
+// console.log(a.charAt());
 
-// minus(10)
+var count = 0
+for (var i = 0; i < a.length; i++) {
+  if (a.charAt(i) === b){
+    count++;
+  }
+}
 
-// IIFI Function (immediately invoked function expression)
+console.log(count)
 
+//===========================================================
 
-// (function (a) {
-//   console.log("IIFI Function",a)
-// })(10)
+var a = "nayan";
 
+var i = 0;
+var j = a.length - 1;
 
-// function sum() {
-//   console.log("Function Called")
-//   return 15;
-// }
+var flag = true
+while (j > i) {
 
-// console.log(sum())
+  if (a.charAt(i) !== a.charAt(j)) {
+    flag = false
+    break
+  }
+  i++;
+  j--;
 
-// user give me two value 10 15 and we need to return sum of the number
+}
 
-
-// var a = 20;
-// var b = 20;
-
-// function sum(a, b) {
-//   return a + b;
-// }
-
-// var ans = sum(a, b)
-// console.log(ans);
-
-// function sum() {
-
-// }
-
-// sum() // Function Call
-
-// Method Call
-
-// var a = {
-//   name: "john",
-//   minus: function () {
-//     console.log("Method Called", this,a)
-//   }
-// }
-
-// a.minus() // Method Call
-
-// var b = a.minus; // 
-
-// b() // Function   
+if(flag){
+  console.log("Palindrome")
+}else{
+  console.log("Not Palindrom")
+}
 
 
-// console.log(window.a)
-// console.log(this.a)
-
-
-// var lname = "Sharma"
-
-// var obj = {
-//   fname: "John",
-//   // lname: "Rai",
-//   fn: function () {
-//     console.log(this.fname + " " +this.lname);
-//   }
-// }
-
-// obj.fn() // John Rai
-// var newFn = obj.fn;
-
-// newFn() // Undefined Sharma
-
-
-// ES6 / 2015
-
-var a = 15;
-
-a = 20;
-
-var a = 35;
-
-// console.log(a);
-
-let b = 15;
-
-b = 20;
-
-// let b = 35; (X)
-
-// console.log(b)
-
-const c = 26;
-
-// c = 96; (X)
-
-// const c = 35; (X)
-
-// console.log(c);
-
-// == vs ===
-
-
-// const cba = "5"
-// const abc = 5;
-
-
-// console.log(cba == abc) // value
-// console.log(cba === abc) // value and type
-
-// Block scope
