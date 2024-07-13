6072bb29bd06e2d3c4f45549ae4c44a91f0ff757
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-07-13 17:32:35 +0530

    DSA

---
+++ main.java
@@ -0,0 +1,74 @@
+// const a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// const n = 6;
+// let b,c;
+
+
+// for(let i=0; i<a.length; i++){
+//     for(let j=0; j<a[i].length; j++){
+//         if(a[i][j] === n){
+//            b = i;
+//            c = j;
+//            break;     
+//         }
+//     }
+// } 
+
+// console.log(b,c)
+
+
+// let sum;
+
+// for(let i=0; i<a.length; i++){
+
+//     for(let j=0; j<a[i].length; j++){
+//         sum += a[i][j]
+//     }
+// } 
+
+// console.log(sum)
+
+
+// a = [[1, 7, 3], [2, 5, 5], [7, 0, 9]]
+
+// let ans = [];
+
+// for (let i = 0; i < a.length; i++) {
+//   let sum = 0;
+//   for (let j = 0; j < a[i].length; j++) {
+//     sum += a[i][j];
+//   }
+//   ans[i] = sum
+// }
+
+// console.log(ans)
+
+
+// a = [[1, 7], [7, 0]]
+
+// let ans = [];
+
+// for (let i = 0; i < a.length; i++) {
+//   let sum = 0;
+//   for (let j = 0; j < a[i].length; j++) {
+//     sum += a[j][i];
+//   }
+//   ans[i] = sum
+// }
+
+// console.log(ans)
+
+
+/* 
+task : sum of the two matrices
+
+a = [[1, 7], [7, 0]] // 2 * 2  
+b = [[2, 3], [5, 2]] // 2 * 2  
+
+ans = [[3, 10], [12, 2]] // 2 * 2 
+
+
+
+
+
+*/
+
