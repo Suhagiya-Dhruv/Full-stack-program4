9a3464a24e503f648aeee809931ba65989888c8d
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-08-05 17:22:28 +0530

    timer

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-// const string = "kjfabkbbababkjiehgx";
+// 
 
-// const a = 'a';
-// const b = 'b';
+// console.log("1");
 
-// let count = 0;
-// let ans = 0;
-
-// for(let i=string.length -1;i>=0;i--){
-//   if(string.charAt(i) === a){
-//     ans += count;
-//   }else if(string.charAt(i) === b){
-//     count++;
-//   }
+// setTimeout(() => {
+//     console.log("2")
+// }, 3000)
+// function callSetTimeout() {
+//     console.log("3");
 // }
 
-// console.log(ans);
+// // // callSetTimeout();
+// const id = setTimeout(callSetTimeout, 2000) // function, (seconds miliseconds)
+// console.log("4")
 
-function findValue(a, b) {
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] === b) {
-            return true;
-        }
+
+// 1000ms -> 1s
+
+// const intervalId = setInterval(() => {
+//     console.log("setInterval")
+// }, 500)
+
+// // clearTimeout(id)
+
+// setTimeout(() => {
+//     clearInterval(intervalId)
+//     console.log("Clear Interval")
+// }, 3000)
+
+
+let timer = 10;
+
+const id = setInterval(() => {
+    timer--;
+    console.log(timer)
+    // element.innerText = timer
+    if(timer === 0){
+        clearInterval(id)
     }
-    return false;
-}
-const array = [-50, -5, -1, 1, 1, 2, 4, 6, 7, 8, 90, 99, 108, 109]
-const sum = 13;
-
-let value = false;
-
-for (let i = array.length - 1; i >= 0; i--) {
-    const a = array[i];
-    const b = sum - a;
-    value = findValue(array, b);
-    if (value) {
-        break;
-    }
-}
-
-value ? console.log("sum possible") : console.log("not");
-
-// task: same question to be completed using two pointer methods
+}, 1000)
