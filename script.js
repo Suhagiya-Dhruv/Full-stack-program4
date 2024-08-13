4c5ee12d7915508c0051ef2ad0e0277753ea83c3
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-08-13 17:02:58 +0530

    Todo list done

--- script.js
+++ script.js
@@ -1,41 +1,80 @@
+
 const list = document.getElementById("list");
 
-const valueList = [];
+const storeValue = JSON.parse(localStorage.getItem("todo")) || [];
 
-function addTask() {
-    const inputElement = document.getElementById("input");
-    valueList.push(inputElement.value);
-    displayTasks();
+if(storeValue.length){
+    renderValue()
 }
 
-function deleteItem(element) {
-    const index = element.dataset.index;
-    valueList.splice(index, 1);
+function updateStorage() {
 
+    localStorage.setItem("todo", JSON.stringify(storeValue));
+
+    // localStorage.setItem("key", "value")
+    // localStorage.getItem("key");
+
+    // setTimeout(() => {
+    //     localStorage.removeItem("key")
+    //     localStorage.clear();
+    // }, 2000)
+
+    // sessionStorage.setItem("key", "value")
+    // sessionStorage.getItem("key");
+
+    // setTimeout(() => {
+    //     sessionStorage.removeItem("key")
+    //     sessionStorage.clear();
+    // }, 2000)
+
+}
+
+function removeItem(element, index) {
+    storeValue.splice(index, 1);
     element.parentNode.remove();
+    updateStorage();
 }
-function displayTasks() {
 
-    let text = "";
-    for (let i = 0; i < valueList.length; i++) {
+function checkbox(element, index) {
+    storeValue[index].status = element.checked;
+    updateStorage();
+    renderValue();
+}
+
+function renderValue() {
+
+    let label = "";
+
+    for (let i = 0; i < storeValue.length; i++) {
+
+        const status = storeValue[i].status;
+
         const temp = `
         <div>
-            <label for="">
-                <input type="checkbox">
-                ${valueList[i]}
+            <label for="" class="${status ? "line" : ""}">
+                <input type="checkbox" onchange="checkbox(this, ${i})" ${status ? "checked" : null}>
+                ${storeValue[i].name}
             </label>
-            <button onclick="deleteItem(this)" data-index="${i}">delete</button>
-        </div>
-        `
+            ${status ? "" : `<button onclick="removeItem(this, ${i})">Delete</button>`}
+        </div>`
 
-        text = text + temp;
+        label += temp;
     }
 
-    list.innerHTML = text;
+    list.innerHTML = label;
     document.getElementById("input").value = "";
 
 }
 
-
-// localStorage
-// sessionStorage
+function addTask() {
+    const value = document.getElementById("input").value;
+    if (value.trim()) { // value.length !== 0 || value !== "" || ""
+        const temp = {
+            name: value,
+            status: false
+        }
+        storeValue.push(temp);
+        updateStorage();
+        renderValue();
+    }
+}
