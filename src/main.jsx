683eede4584bf7b0b518fb57b4725103563b27a1
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-09-04 16:53:43 +0530

    React intro

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import Layout from './Components/layout'
+
+createRoot(document.getElementById('root')).render(
+  <Layout name="123">
+    <h1>Main  2</h1>
+  </Layout>
+  
+)
