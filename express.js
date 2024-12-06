6b1de2e512f8cbfa0e4a2d19a39a353a21c17c42
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-12-06 17:20:53 +0530

    update

--- express.js
+++ express.js
@@ -1,19 +1,34 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 
-const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+const data = [];
 
-app.get('/', (request, resposne) => {
-    resposne.json(data)
+// Middleware
+app.use(express.json());
+
+app.get('/list', (req, res) => {
+    res.json({
+        data: data,
+        message: 'Data fetched successfully'
+    });
 })
 
-app.get('/:userId', (request, resposne) => {
-    const userId = request.params.userId
-    const filterData = data.find(item => item.id == userId)
-    resposne.json(filterData)
-})
+app.post('/add', (req, res) => {
+    // email check 
+    const user = data.find(user => user.email === req.body.email);
 
+    if (user) {
+        return res.json({ message: 'Email already exists' });
+    }
+    // user_id genarate
+    const payload = {
+        user_id: Date.now(),
+        ...req.body,
+    }
+
+    data.push(payload)
+    return res.json({ message: 'Data added..' })
+})
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
