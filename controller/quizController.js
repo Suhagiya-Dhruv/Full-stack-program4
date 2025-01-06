a5fd16d16e2a0d135d022ec39e4b7e56fc3b0320
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2025-01-06 18:07:28 +0530

    Quiz 4

--- controller/quizController.js
+++ controller/quizController.js
@@ -1,64 +1,140 @@
-export const allQuizList = async (req, res) => {
-    try{
+import quizModel from "../models/quizModel.js"
 
-    }catch(err){
+export const allQuizList = async (req, res) => {
+    try {
+
+        const quiz = await quizModel.find();
+
+        return res.json({
+            data: quiz
+        })
+
+    } catch (err) {
         res.status(500).json({
             message: 'Internal Server Error',
             status: false,
             data: null
         })
-    
+
     }
 }
 
 export const singleQuizList = async (req, res) => {
-    try{
+    try {
 
-    }catch(err){
+        const id = req.params.id;
+        const quiz = await quizModel.findById(id);
+
+        if (!quiz) {
+            return res.status(404).json({
+                message: "Quiz not found..!"
+            })
+        }
+
+        return res.json({
+            data: quiz
+        })
+    } catch (err) {
         res.status(500).json({
             message: 'Internal Server Error',
             status: false,
             data: null
         })
-    
+
     }
 }
 
 export const createQuiz = async (req, res) => {
-    try{
+    try {
+        const { name, startTime, duration, category } = req.body;
 
-    }catch(err){
+        if (!name || !startTime || !duration || !category) {
+            return res.status(400).json({
+                message: 'All fields are required',
+                status: false,
+                data: null
+            })
+        }
+
+        const quiz = await quizModel.create({ ...req.body, startTime: new Date(startTime).toISOString() });
+
+        return res.status(201).json({
+            message: 'Quiz created successfully',
+            status: true,
+            data: quiz
+        })
+
+    } catch (err) {
         res.status(500).json({
             message: 'Internal Server Error',
             status: false,
-            data: null
+            data: err.message
         })
-    
+
     }
 }
 
 export const updateQuiz = async (req, res) => {
-    try{
+    try {
+        const id = req.params.id;
 
-    }catch(err){
+        const quiz = await quizModel.findById(id);
+        if (!quiz) {
+            return res.status(404).json({
+                message: 'Quiz not found..!',
+                status: false,
+                data: null
+            })
+        }
+
+        for (const key in req.body) {
+            quiz[key] = req.body[key]
+        }
+
+        await quiz.save();
+
+        return res.json({
+            message: "Quiz updated",
+            data: quiz,
+            status: true
+        })
+
+    } catch (err) {
         res.status(500).json({
             message: 'Internal Server Error',
             status: false,
             data: null
         })
-    
+
     }
 }
 
 export const deleteQuiz = async (req, res) => {
-    try{
+    try {
+        const id = req.params.id;
 
-    }catch(err){
+        const quiz = await quizModel.findByIdAndDelete(id)
+
+        if (!quiz) {
+            return res.status(404).json({
+                message: 'Quiz not found..!',
+                status: false,
+                data: null
+            })
+        }
+
+        return res.json({
+            message: 'Quiz deleted',
+            status: false,
+            data: null
+        })
+
+    } catch (err) {
         res.status(500).json({
             message: 'Internal Server Error',
             status: false,
             data: null
         })
-    
+
     }
 }
--- express.js
+++ express.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { dbConnection } from "./config/dbConnection.js";
 import userRouter from "./routers/userRouter.js";
 import quizRouter from "./routers/quizRouter.js";
+import questionRouter from "./routers/questionRouter.js";
 
 const app = express();
 dbConnection()
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/quiz', quizRouter);
+app.use('/api/v1/question', questionRouter);
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
